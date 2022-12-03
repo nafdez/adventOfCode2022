482e1ef01dc8c3b88a9ec64ae0111bc40dfa5bda
ignaciofp60 <ignaciofp60@educastur.es>
2022-12-03 17:25:35 +0100

    Day 3 - Part 2 completed

--- adventOfCode2022/src/day3/Part2_solution.java
+++ adventOfCode2022/src/day3/Part2_solution.java
@@ -5,15 +5,16 @@ import java.io.IOException;
 import java.util.Scanner;
 import java.util.Vector;
 
-public class Part1_solution {
+public class Part2_solution {
 
 	public static void main(String[] args) {
 		// Variables
-		String[] hvStr = new String[2];
+		String[] hvStr = new String[3];
+		int cAux = 0;
 		int result = 0;
 		int prioraz = 0;
 		int priorAZ = 0;
-		String c1a = "";
+		String badge = "";
 		String az = "";
 		String AZ = "";
 		
@@ -35,14 +36,17 @@ public class Part1_solution {
 	            AZ+=c;
 	        
 	     // Separando el input en dos
-	        for(int i=0; i<data.size(); i++) {       	
-	        	hvStr = stringHalving(data, i);
+	        for(int i=0; i<data.size(); i=i+3) {
+	        	for(int j=0; j<3; j++) 
+	        		hvStr[j] = data.elementAt(cAux++);
+
+	        	String c1a = String.valueOf(searchInIndex(hvStr[0], hvStr[1]));
+	        	badge = String.valueOf(searchInIndex(c1a, hvStr[2])[0]); // index 0, porque puede que la común se repita
 	        	
-	        	c1a = String.valueOf(searchInIndex(hvStr[0], hvStr[1]));
-	        	
+	        	System.out.println(badge);
 	        	// Establecemos prioridades del 1 al 26 para a-z y 27-52 para A-Z
-	        	prioraz = az.indexOf(c1a)+1;
-	        	priorAZ = AZ.indexOf(c1a)+27;
+	        	prioraz = az.indexOf(badge)+1;
+	        	priorAZ = AZ.indexOf(badge)+27;
 	        	if(priorAZ==26) // Como le sumamos 27, para hacerlo más fácil si el resultado da 26, será 0
 	        		priorAZ=0;
 	        	result += prioraz + priorAZ;
@@ -57,19 +61,25 @@ public class Part1_solution {
 
 	}
 	
-	static String[] stringHalving(Vector <String> data, int i) {
-		String[] strAux = new String[2];
-		strAux[0] = data.elementAt(i).substring(0, data.elementAt(i).length()/2);
-    	strAux[1] = data.elementAt(i).substring(data.elementAt(i).length()/2);
+	static String[][] stringGroups(Vector <String> data, String[][] strAux, int i, int j) {
+		strAux[j][0] = data.elementAt(i).substring(0, data.elementAt(i).length()/2);
+    	strAux[j][1] = data.elementAt(i).substring(data.elementAt(i).length()/2);
 		return strAux;
 	}
 	
-	static char searchInIndex(String s1a, String s1b) {
-		char c2 = ' ';
-		for(int j=0; j<s1b.length(); j++) {
+	static char[] searchInIndex(String str01, String str02) {
+		// Evaluo cual string es más largo para que c2 no se quede sin espacio nunca
+		// Dado el caso que si los 2 string son iguales, c2 se llenaría muy rápido
+		int l = str02.length();
+		if(str01.length()>str02.length())
+			l = str01.length();
+		// Función principal
+		char c2[] = new char[l];
+		int c = 0;
+		for(int j=0; j<str02.length(); j++) {
     		try { 
-    			c2 = s1b.charAt(s1b.indexOf(s1a.charAt(j)));
-    		}catch(StringIndexOutOfBoundsException e) {}
+    			c2[c++] = str01.charAt(str01.indexOf(str02.charAt(j)));
+    		}catch(StringIndexOutOfBoundsException e) {c--;}
     	}
 		return c2;
 		
