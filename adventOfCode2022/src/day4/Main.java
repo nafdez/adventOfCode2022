b890af52c04a3057ee85b84978c8cc894db584fe
ignaciofp60 <ignaciofp60@educastur.es>
2022-12-04 18:44:25 +0100

    Day4 part 1 completed

---
+++ adventOfCode2022/src/day4/Main.java
@@ -0,0 +1,92 @@
+package day4;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.Vector;
+
+public class Main {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		int c = 0;
+		try {
+			FileInputStream file = new FileInputStream(
+					"C:/Users/Nacho/git/adventOfCode2022/adventOfCode2022/src/day4/Input_test.txt");
+			Scanner scanner = new Scanner(file);
+			Vector<String> data = new Vector<>();
+			while (scanner.hasNext()) {
+				String temp = scanner.nextLine().replaceAll(",", "-");
+				data.addElement(temp);
+				c++;
+			}
+			int[][] intData = toInt(data, c);
+
+			int partOverlaps = 0;
+			int fullOverlaps = 0;
+			for (int i = 0; i < intData.length; i++) {
+				String par1 = zoneToClean(intData, i, 2);
+				String par2 = zoneToClean(intData, i, 1);
+				
+				if (fullyOverlaps(intData, i))
+					fullOverlaps++;
+				
+				if (overlaps(par1, par2))
+					partOverlaps++;
+			}
+			
+			System.out.println("La cantidad de superposiciones TOTALES entre las parejas son: " + fullOverlaps);
+			System.out.println("La cantidad de superposiciones entre las parejas son: " + partOverlaps);
+
+			scanner.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	static int[][] toInt(Vector<String> v, int c) {
+		int[][] aux = new int[c][4];
+		for (int i = 0; i < c; i++) {
+			String[] test = v.elementAt(i).split("-");
+			for (int j = 0; j < 4; j++)
+				aux[i][j] = Integer.valueOf(test[j]);
+		}
+
+		return aux;
+
+	}
+
+	static String zoneToClean(int[][] intData, int i, int value) {
+		String zone = "";
+		int c = 1; // contador
+		int auxC = 0; // nuevo contador
+		// aux posición 1 del primer par. // c la 2 pos.
+		// se suma 2 para pasar al siguiente par
+
+		if (value == 1) { // Con value sabemos si es el primero o el segundo del par;
+			c = 3;
+			auxC = 2;
+		}
+		for (int x = intData[i][auxC]; x < intData[i][c] + 1; x++) {
+			zone = zone.concat(String.valueOf(x)+" ");
+		}
+
+		return zone;
+	}
+
+	static boolean overlaps(String par1, String par2) {
+		if (par1.contains(par2) || par2.contains(par1))
+			return true;
+		return false;
+	}
+	
+	static boolean fullyOverlaps(int[][] intData, int i) {
+		if (intData[i][0]<=intData[i][2] && intData[i][1]>=intData[i][3])
+			return true;
+		else if(intData[i][0]>=intData[i][2] && intData[i][1]<=intData[i][3])
+			return true;
+		return false;
+	}
+
+}
