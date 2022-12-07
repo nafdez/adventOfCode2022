9b1150ac20cfdb75d0b7c0f825437b63d34668d5
ignaciofp60 <ignaciofp60@educastur.es>
2022-12-07 14:20:08 +0100

    wqedfwe

--- adventOfCode2022/src/day6/Solution_Part1.java
+++ adventOfCode2022/src/day6/Solution_Part1.java
@@ -1,7 +1,10 @@
 package day6;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 import java.util.Vector;
@@ -12,7 +15,13 @@ public class Solution_Part1 {
 	static int contador = 0;
 	static int pos = markSize-1;
 	
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.print("Introduzca el tamaño del marcador del paquete de datos: ");
+		markSize = Integer.valueOf(in.readLine());
+		System.out.println();
+		
 		FileInputStream f = new FileInputStream("C:/Users/Nacho/git/adventOfCode2022/adventOfCode2022/src/day6/Input.txt");
 		Scanner sc = new Scanner(f);
 		
