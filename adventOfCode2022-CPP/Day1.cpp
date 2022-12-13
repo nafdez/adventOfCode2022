5291063d2650661e96559ed17ff1369b9a12d079
ignaciofp60 <ignaciofp60@educastur.es>
2022-12-13 14:39:40 +0100

    continuan los testes

--- adventOfCode2022-CPP/Day1.cpp
+++ adventOfCode2022-CPP/Day1.cpp
@@ -7,6 +7,7 @@
 
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
 
 int main(){
@@ -14,9 +15,18 @@ int main(){
 	ifstream file(fname.c_str());
 
 	string line;
+	int c;
+	int in[1000];
+	int sum;
+	int cal;
+
 	// Obteniendo una línea del archivo y almacenarla en la variable "line"
-	while(getline(file, line)){
-		cout << "line: " << line << endl;
+	while(getline(file, line)) {
+		in[c] = static_cast<int>(line);
+	}
+
+	for(int i = 0; i<sizeof(in); i++) {
+		cout << in[i] << endl;
 	}
 
 	return 0;
