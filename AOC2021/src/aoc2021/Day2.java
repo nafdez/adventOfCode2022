8567ae36291ee98973001907168fe8cc32d41eb2
nafdez <fzp.nacho@gmail.com>
2022-12-17 17:34:37 +0100

    Day2 - Completed

--- AOC2021/src/aoc2021/Day2.java
+++ AOC2021/src/aoc2021/Day2.java
@@ -1,10 +1,41 @@
 package aoc2021;
 
+import java.util.Scanner;
+
 public class Day2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(Day2.class.getResourceAsStream("/day2Input.txt"));
+
+		int horizontalPos = 0; // Part One
+		int depth = 0;
+		int PThorizontalPos = 0; // Part Two
+		int PTdepth = 0;
+		int aim = 0;
+		
+		while (sc.hasNextLine()) {
+			String line = sc.nextLine();
+			String[] cmd = line.split(" "); // Separa el input en dos, el comando (cmd[0]) y el valor de este (cmd[1]
+
+			switch (cmd[0]) {
+			case ("forward"):
+				horizontalPos += Integer.valueOf(cmd[1]);
+				PThorizontalPos += Integer.valueOf(cmd[1]);
+				PTdepth += aim * Integer.valueOf(cmd[1]);
+				break;
+			case ("down"):
+				depth += Integer.valueOf(cmd[1]);
+				aim += Integer.valueOf(cmd[1]);
+				break;
+			case ("up"):
+				depth -= Integer.valueOf(cmd[1]);
+				aim -= Integer.valueOf(cmd[1]);
+				break;
+			}
+		}
+
+		System.out.println("[Part_One] Valor final: " + (horizontalPos * depth));
+		System.out.println("[Part_Two] Valor final: " + (PThorizontalPos * PTdepth));
 
 	}
-
 }
